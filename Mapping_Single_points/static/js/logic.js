016cc56d53b3f6b43780dc1e25c7661ac6eafaf5
Rauloigs <rauloigs@gmail.com>
2022-03-27 16:42:05 -0600

    m

--- Mapping_Single_points/static/js/logic.js
+++ Mapping_Single_points/static/js/logic.js
@@ -5,18 +5,25 @@ console.log("working");
 // mapid references de id tag in our <div> & 4 is the zoom level. 
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// Alternative instead of using SetView
-// let map = L.map("mapid", {
-    // center: [
-        // 40.7, -94.5
-    // ],
-    // zoom: 4
-// }); 
+// Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-// This method is useful when we need to add multiple tile layers, or a background image of our map(s)
+// Add a marker-cirle to the map for Los Angeles, California. 
+// L.circle([34.0522, -118.2437], {
+//     radius: 300,
+//     fillColor: '',
+//     color: 'black'
+// }).addTo(map);
+
+// Add a circle to the map.
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: "yellow"
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     // id: 'mapbox/streets-v11',
