bd56d6a6d285507c97c99689d2c516146af71733
Rauloigs <rauloigs@gmail.com>
2022-03-28 09:55:29 -0600

    m

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,20 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-// mapid references de id tag in our <div> & 4 is the zoom level. 
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-
-// Alternative instead of using SetView
-// let map = L.map("mapid", {
-    // center: [
-        // 40.7, -94.5
-    // ],
-    // zoom: 4
-// }); 
-
-// This method is useful when we need to add multiple tile layers, or a background image of our map(s)
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -24,5 +10,66 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     // zoomOffset: -1,
     accessToken: API_KEY
 });
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// streets.addTo(map);
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [streets]
+});
+
+// Pass our map layers into our layer control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accesing the Toronto neighborhoods GeoJSON URL
+let torontoHoods = "https://raw.githubusercontent.com/Rauloigs/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
+
+// Create Style
+let myStyle = {
+    color: "blue",
+    fillColor: "yellow",
+    weight: 1
+}
+// color: "#ffffa1",
+
+
+// Creating a GeoJSON layer with the retrieved data.
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data, {
+        style: myStyle,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+        }
+    }).addTo(map);
+});
+
+// // Grabbing GeoJSON data. 
+// d3.json(torontoData).then(function(data) {
+//     console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJson(data, {
+//     style: myStyle,
+//     onEachFeature: function(feature, layer) {
+//         layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+//     }
+// })
+// .addTo(map);
+// });
